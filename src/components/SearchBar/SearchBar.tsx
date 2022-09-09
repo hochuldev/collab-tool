49678c643f1a6481c0571f477ba8b349c61ae440
Hochul Song <hochuldev@naver.com>
2022-09-09 13:00:37 +0900

    SearchBar 컴포넌트의 재사용성을 높이기 위해, className을 props로 추가함

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./SearchBar.css"
 import searchIcon from "../../assets/search-icon.svg"
 
-const SearchBar = () => {
+const SearchBar = ({className}: {className:string}) => {
   // TODO: 옵션 클릭시 옵션 창이 나타나도록 구현해야함.
   // TODO: 상태와 value 값이 같도록 구현해야함.
+  // TODO: input에 텍스트를 넣었을 때, 텍스트 색이 파란색으로 바뀌도록 구현해야함.
   return(
-    <div className="search-bar">
+    <div className={`search-bar ${className}`}>
       <img src={searchIcon} alt="검색 아이콘" className="search-bar__icon"></img>
       <input type="text" placeholder="검색어를 입력해주세요" className="search-bar__input"></input>
       <button type="button" className="search-bar__button"><span className="search-bar__divider" aria-hidden="true">|</span> 옵션</button>
