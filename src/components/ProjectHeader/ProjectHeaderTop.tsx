ebff296c69ac01a211330057bdb115701833f001
Hochul Song <hochuldev@naver.com>
2022-09-11 18:21:14 +0900

    html 버튼 요소가 아닌, 버튼 컴포넌트로 버튼을 대체함

--- src/components/ProjectHeader/ProjectHeaderTop.tsx
+++ src/components/ProjectHeader/ProjectHeaderTop.tsx
@@ -2,9 +2,12 @@ import React, { MouseEventHandler } from "react"
 import { useState } from "react"
 import bookmark from "../../assets/bookmark-icon.png"
 import threeDotsIcon from "../../assets/three-dots-icon.svg"
+import Button from "../Button/Button"
+// import companyIcon from "../../assets/company-icon.svg"
 import "./ProjectHeaderTop.css"
 
 const ProjectHeaderMenu = ({projectName, hidden}: {projectName:string, hidden: boolean}) => {
+  // TODO: 사용자 권한에 따라서, 다른 메뉴를 보여줘야 함. 관리자와 일반 사용자는 다른 메뉴 ui를 갖는다.
   return (
     <div className={"project-header__menu" + (hidden ? " hidden" : "")}>
       <div className="project-header__menu-title">
@@ -21,6 +24,7 @@ const ProjectHeaderMenu = ({projectName, hidden}: {projectName:string, hidden: b
 }
 
 const ProjectHeaderTop = ({projectName, projectColor}: {projectName: string, projectColor: string}) => {
+  //TODO: headerTop의 크기가 px단위로 고정돼있는데, 안된다. 프로젝트 제목의 길이에 따라서 길이가 변하도록 만든 뒤, 최대 길이를 정하고, ellipsis 적용하자.
   const [hidden, setHidden] = useState(true)
 
   const toggleHidden:MouseEventHandler = (e) => {
@@ -30,12 +34,12 @@ const ProjectHeaderTop = ({projectName, projectColor}: {projectName: string, pro
   return (
     <div className="project-header__top">
       <div className= {`project-header__color-box_${projectColor} project-header__color-box`}></div>
-      <button type="button" className="project-header__bookmark">
+      <Button width="18px">
         <img className="project-header__bookmark-icon" src={bookmark} alt="북마크 버튼" />
-      </button>
-      <button className="project-header__three-dots" type="button"  onClick={toggleHidden}>
-        <img className="project-header__three-dots" src={threeDotsIcon} alt="프로젝트 설정 버튼"></img>
-      </button>
+      </Button>
+      <Button width="18px">
+        <img className="project-header__three-dots" src={threeDotsIcon} alt="프로젝트 설정 버튼" onClick={toggleHidden}></img>
+      </Button>
       <ProjectHeaderMenu projectName={projectName} hidden={hidden}></ProjectHeaderMenu>
       <h1>{projectName + " 업무 공유방"}</h1> {/* TODO: 아이콘 추가, 스타일링 추가 */}
     </div>
