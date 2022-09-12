867417d01f1abc80cac958152daa5b245c579575
Hochul Song <hochuldev@naver.com>
2022-09-12 17:56:24 +0900

    사이드바의 재사용성과 확장성을 확보하기 위해, props 규칙을 개정하여 적용함

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { JsxElement } from "typescript";
+import React, {ReactNode} from "react";
 import "./Sidebar.css"
+import Button from "../Button/Button";
+interface SidebarProps {
+  width: string,
+  theme: 'light' | 'dark',
+  className: string,
+  children: ReactNode
+}
 
-const Sidebar = ({wInPx, className}: {wInPx: string, className: string}) => {
+const determineColor = (theme: "light" | "dark") => {
+  if(theme === "light") {
+    return {
+      backgroundColor: "rgb(238, 238, 238)",
+      color: "rgb(204,204,204)"
+    }
+  } else {
+    return {
+      backgroundColor: "rgb(44, 42, 52)",
+      color: "black",
+    }
+  }
+}
+
+const Sidebar = ({width, className, theme, children}: SidebarProps) => {
+
+  const {backgroundColor, color} = determineColor(theme)
   return (
-    <aside className={"sidebar " + className} style={{width: {wInPx}}}>
-      
+    <aside className={"sidebar " + className} style={{width: width, backgroundColor: backgroundColor, color: color}}>
+      {children}
     </aside>
   )
 }
--- src/components/Sidebar/SidebarMain.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SidebarMain = () => {
-  return (
-    <button className="sidebar__new-project-button">새 프로젝트</button>
-  )
-}
-
-export default SidebarMain
