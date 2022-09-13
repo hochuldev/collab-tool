781f502122e3bb55aa7df91871aa7bf9706ac46a
Hochul Song <hochuldev@naver.com>
2022-09-13 18:11:38 +0900

    테스트를 위해 Sidebar 컴포넌트를 App.tsx에 추가함

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import ProjectHeader from './components/ProjectHeader/ProjectHeader';
+import HomeSidebar from './pages/ProjectHome/HomeSidebar/HomeSidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ProjectHeader></ProjectHeader>
+        <ProjectHeader />
+        <HomeSidebar />
       </div>
     </BrowserRouter>
   );
