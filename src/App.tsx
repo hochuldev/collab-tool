7bddc0bc457d939527bb57dd7ce57bd698058e24
Hochul Song <hochuldev@naver.com>
2022-09-20 17:48:33 +0900

    로그인 페이지가, 모바일에서 세로 중앙 정렬되도록 개선함

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import ProjectHeader from './components/ProjectHeader/ProjectHeader';
 import HomeSidebar from './pages/ProjectHome/HomeSidebar/HomeSidebar';
-import SearchBarOption from './components/SearchBar/SearchBarOption';
+import WorkReport from './components/WorkReport/WorkReport';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      {/* <div className="App">
         <ProjectHeader />
         <HomeSidebar />
       </div>
+      <WorkReport /> */}
+      <LoginPage />
     </BrowserRouter>
   );
 }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -15,6 +15,7 @@ const LoginPage = () => {
       <input className="login__input login__input_password" type="password" placeholder="비밀번호"></input>
       <Link to="find-password" className="login__button login__button_find-password">비밀번호 찾기</Link>
       <Button className="login__button login__button_login" width="100%">로그인</Button>
+      <Link to="register" className="login__button login__button_register"><div className="login__register-text">회원가입</div></Link>
       <Button className="login__button login__button_auto-login" width="fit-content" onClick={toggleAutoLogin}>
         <div className={"login__icon_auto-login " + (autoLogin ? "login__icon_auto-login_true" : "login__icon_auto-login_false")}>
           <div className={"login__icon_check " + (autoLogin ? "login__icon_check_true" : "login__icon_check_false")}></div>
