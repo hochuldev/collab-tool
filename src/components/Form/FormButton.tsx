c4ae957f1f5828adcc2ddf8e0f03979c49d64b8b
Hochul Song <hochuldev@naver.com>
2022-09-22 20:03:34 +0900

    'Form' 컴포넌트의 서브 컴포넌트인 'FormButton'을 구현함

--- src/components/Form/FormButton.tsx
+++ src/components/Form/FormButton.tsx
@@ -1,12 +1,37 @@
-import Button from "../Button/Button"
+import { ReactChildrenProp } from '../../typeDeclaration'
+import { Link } from 'react-router-dom'
+import Button from '../Button/Button'
 import './FormButton.css'
 
-interface FormButtonProps {
-  value: string
+interface FormButtonProps extends ReactChildrenProp {
+  onClick?: React.MouseEventHandler
+  to?: string
+  id?: string
 }
 
-const FormButton = ({value}: FormButtonProps) => {
-  <Button className="login__button" width="100%">{value}</Button>
+const FormButton = ({ onClick, to, id, children }: FormButtonProps) => {
+  if (to === undefined) {
+    if (onClick === undefined)
+      throw new Error(
+        'to props를 전달하지 않으면, onClick props를 전달하여, 클릭 이벤트를 처리해야 합니다.'
+      )
+    return (
+      <Button id={id} className="form__button" width="100%" onClick={onClick}>
+        {children}
+      </Button>
+    )
+  } else {
+    return (
+      <Link
+        id={id}
+        to={to}
+        className="form__button form__button_is-link"
+        onClick={onClick}
+      >
+        <div>{children}</div>
+      </Link>
+    )
+  }
 }
 
 export default FormButton
